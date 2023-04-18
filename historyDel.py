1b6ac051f87b7f306d432f2b7222a3204eca8ffe
Tumbleweed-64 <117474637+Tumbleweed-64@users.noreply.github.com>
2023-04-18 16:27:40 -0700

    Update historyDel.py

--- historyDel.py
+++ historyDel.py
@@ -4,16 +4,16 @@ userName = ""
 historyPath = ""
 if osType.lower() == "windows":
     userName = input("What's your username? (used for concatenating in filepath) ")
-    historyPath = "C:\\Users\\" + username + "\\AppData\\Local\\Google\\Chrome\\User Data\\Default"
+    historyPath = "C:\\Users\\" + username + "\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\History"
     os.remove(historyPath)
     print("History deleted. Tool made by Tumbleweed-64 on GitHub.")
 elif osType.lower() == "macos":
     userName = input("What's your username? (used for concatenating in filepath) ")
-    historyPath = "/Users/" + userName + "/Library/Application Support/Google/Chrome/Default"
+    historyPath = "/Users/" + userName + "/Library/Application Support/Google/Chrome/Default/History/"
     os.remove(historyPath)
     print("History deleted. Tool made by Tumbleweed-64 on GitHub.")
 elif osType.lower() == "linux":
     userName = input("What's your username? (used for concatenating in filepath) ")
-    historyPath = "/home/" + userName + "/.config/google-chrome/Default"
+    historyPath = "/home/" + userName + "/.config/google-chrome/Default/History"
     os.remove(historyPath)
     print("History deleted. Tool made by Tumbleweed-64 on GitHub.")
